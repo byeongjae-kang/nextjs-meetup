541b9e0dee79176900a6aa2bd3d4b34c735e335d
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-09-13 22:20:23 -0700

    create home page component and dummy data, import and render MeetupList component

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "The First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Science_World_at_TELUS_World_of_Science.jpg/1599px-Science_World_at_TELUS_World_of_Science.jpg",
+    address: "1455 Quebec St, Vancouver, BC V6A 3Z7",
+    description: "This is our first meetup"
+  },
+  {
+    id: "m2",
+    title: "The Second Meetup",
+    image:
+      "https://www.aldergrovestar.com/wp-content/uploads/2020/03/20995280_web1_200318-LAT-Langley-City-Hall-timms_1-1024x683.jpg",
+    address: "20399 Douglas Crescent, Langley, BC V3A 4B3",
+    description: "This is our second meetup"
+  }
+];
+
+export default function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
