843b9ce383860348e1d8e34e73c17600efa19679
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-09-13 23:04:05 -0700

    create component to render based on dynamic url parameter, pass hard coded props for now

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,13 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+export default function MeetupDetails() {
+  return (
+    <MeetupDetail
+      id="m1"
+      title="The First Meetup"
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Science_World_at_TELUS_World_of_Science.jpg/1599px-Science_World_at_TELUS_World_of_Science.jpg"
+      address="1455 Quebec St`` Vancouver`` BC V6A 3Z7"
+      description="This is our first meetup"
+    />
+  );
+}
