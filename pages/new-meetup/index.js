c7f216e5f46e1d0deaa6b91489ca019632dec0e7
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-09-14 22:25:21 -0700

    add http request to save new meetup to the database

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,8 +1,19 @@
+import { useRouter } from "next/router";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 export default function NewMeetup() {
-  function addMeetupHandler(enteredMeetupData) {
-    console.log(enteredMeetupData);
+  const router = useRouter();
+
+  async function addMeetupHandler(enteredMeetupData) {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    router.push("/");
   }
   return (
     <div>
