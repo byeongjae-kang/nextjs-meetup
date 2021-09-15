9bf483b93d0387cdcf8259091ae2b94c2276a2cd
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-09-15 08:14:30 -0700

    fixed fallback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -16,7 +16,7 @@ export async function getStaticPaths() {
 
   client.close();
   return {
-    fallback: false,
+    fallback: "blocking",
     paths: result.map((meetup) => ({
       params: {
         meetupId: meetup._id.toString()
