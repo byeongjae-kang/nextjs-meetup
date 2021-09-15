e3afc1de3de5da06b4b1a5eef8b3defe178b8406
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-09-14 21:08:28 -0700

    prerender with data

--- pages/index.js
+++ pages/index.js
@@ -19,11 +19,23 @@ const DUMMY_MEETUPS = [
   }
 ];
 
-export function getStaticProps() {
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS
+//     }
+//   };
+// }
+
+export async function getStaticProps() {
   return {
     props: {
       meetups: DUMMY_MEETUPS
-    }
+    },
+    revalidate: 10
   };
 }
 
